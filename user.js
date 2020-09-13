1800bd6e2e3e982aad01abfede87c1faf79ba5f3
Jerry Lokjianming <35756679+JerryLokjianming@users.noreply.github.com>
2020-09-13 13:46:17 +0000

    Update user.js

--- user.js
+++ user.js
@@ -1297,3 +1297,4 @@ user_pref("browser.urlbar.suggest.topsites", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 user_pref("startup.homepage_welcome_url", "about:blank");
+user_pref("browser.urlbar.maxRichResults", 0;
